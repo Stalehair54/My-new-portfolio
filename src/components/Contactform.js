8919b8ac7191339828e18dc5df36dc7ea84e12b1
Stalehair54 <nickgod123@yahoo.com>
2022-09-12 14:31:49 -0400

    Fixed email

--- src/components/Contactform.js
+++ src/components/Contactform.js
@@ -1,37 +1,38 @@
-import React, { useState } from "react";
-
-const FORM_ENDPOINT = "";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 const ContactForm = () => {
-  const [submitted, setSubmitted] = useState(false);
-  const handleSubmit = () => {
-    setTimeout(() => {
-      setSubmitted(true);
-    }, 100);
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_t2io3nn",
+        "template_0rdhtoh",
+        form.current,
+        "bM6cCJbPBOJcaxfRF"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    form.current.reset();
   };
 
-  if (submitted) {
-    return (
-      <>
-        <div className="text-2xl">Thank you!</div>
-        <div className="text-md">We'll be in touch soon.</div>
-      </>
-    );
-  }
-
   return (
-    <form
-      action={FORM_ENDPOINT}
-      onSubmit={handleSubmit}
-      method="POST"
-      target="_blank"
-    >
+    <form onSubmit={sendEmail} ref={form}>
       <div className="mb-3 pt-0">
         <input
           type="text"
           placeholder="Your name"
           name="name"
-          className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-[#313131]   text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+          className="px-3 py-3 placeholder-gray-400 text-white relative bg-[#313131]   text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
           required
         />
       </div>
@@ -40,7 +41,7 @@ const ContactForm = () => {
           type="email"
           placeholder="Email"
           name="email"
-          className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-[#313131]  rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+          className="px-3 py-3 placeholder-gray-400 text-white relative bg-[#313131]  rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
           required
         />
       </div>
@@ -53,12 +54,11 @@ const ContactForm = () => {
         />
       </div>
       <div className="mb-3 pt-0">
-        <button
+        <input
           className=" border border-sky-500 text-[#08fdd8] active:bg-blue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
           type="submit"
-        >
-          Send a message
-        </button>
+          value="Send Message"
+        />
       </div>
     </form>
   );
